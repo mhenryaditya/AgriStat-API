d5dca39f90d0c7830a738d6fdd5b1ff195ce7e2c
David Jonathan Tjong <2211016310007@mhs.ulm.ac.id>
2025-03-11 23:39:06 +0800

    update api and add cors

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return response()->json([
@@ -16,4 +17,9 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware(['auth:api']);
 Route::post('/auth/refresh', [AuthController::class, 'refresh']);
 Route::post('/user/register', [AuthController::class, 'register'])->middleware(['auth:api']);
-Route::get('/user/profile/{filename}', [AuthController::class, 'getProfileImage'])->middleware(['auth:api']);
+Route::get('/user/profile/{filename}', [AuthController::class, 'getProfileImage'])->middleware(['auth:api']); 
+
+// check if current user is valid from token
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return response()->json($request->user());
+});
